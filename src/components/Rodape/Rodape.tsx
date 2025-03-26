e7420c4400d8ffda8036ca88bedcb964c1e2c395
Tiago Filho <tiagofilho.stz@gmail.com>
2025-03-26 14:47:55 -0300

    primeiro commit do projeto

--- src/components/Rodape/Rodape.tsx
+++ src/components/Rodape/Rodape.tsx
@@ -3,8 +3,8 @@ import estilo from './Rodape.module.css';
 function Rodape() {
     return (
         <footer className={estilo.rodape}>
-            <p>Desenvolvido por: Aula React</p>
-            <p>Copyright</p>
+            <p>Desenvolvido por: Tiago Filho</p>
+            <p>Copyright ©</p>
         </footer>
     );
 }
